4ba63f2fb51f53e6a210177afbb9bb89c7e6a362
harryseow94 <harryseow94@gmail.com>
2017-11-23 14:53:06 +0800

    Updated to detect PDF/PPT before conversion

--- ppt2png.js
+++ ppt2png.js
@@ -1,26 +1,34 @@
 var exec = require('child_process').exec,
     fs = require('fs');
+    path = require('path');
 
 
 var ppt2png = function(input, output, callback) {
-  exec('unoconv -f pdf -o ' + output + '.pdf ' + input, 
+  if(path.extname(input) == ".ppt" || path.extname(input) == ".pptx"){
+    var pdfPath = output;
+    exec('unoconv -f pdf -o ' + pdfPath + '.pdf ' + input, 
     function( error, stdout, stderr) {
       if (error) {
         callback(error);
         return;
       }
-
-      pdf2png(output+'.pdf', output, function(err){
-        fs.unlink(output+'.pdf', function(err) {
-          if(err) {
-            console.log(err);
-          }
-
-        });
-
-        callback(err);       
-      });
     });
+  }
+  else if(path.extname(input) == ".pdf"){
+    var pdfPath = input.substr(0, input.lastIndexOf('.')) || input;
+  }  
+  
+  pdf2png(pdfPath+'.pdf', output, function(err){
+    fs.unlink(pdfPath+'.pdf', function(err) {
+      if(err) {
+        console.log(err);
+      }
+
+    });
+
+    callback(err);       
+  });
+
 }
 
 var pdf2png = function(input, output, callback) {
